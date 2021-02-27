4821901cf99f21171717013dd2bfd92957021a4b
Mariah Morales <mjustine622@gmail.com>
2021-02-26 20:19:08 -0500

    commit tree a03f03538a826cbaf1ed880d9881e361db0dbc27

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
-#write your code here
-
-def countdown
+x = 10 
+def countdown(x)
+    while x > 0
+        puts "#{x} SECOND(S)!"
+        x -= 1 
+    end
+    "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(x)
+    while x > 0 
+        puts "#{x} SECONDS(S)!"
+        x -= 1
+        sleep (2)
+    end
+end	
